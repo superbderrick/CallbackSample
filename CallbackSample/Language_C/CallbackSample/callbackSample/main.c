2344944bec02188689a9de49583de810a709dfa7
Kang Jinyeoung <derrick@Kangs-MacBook-Pro.local>
2015-10-18 23:09:10 +0900

    complete c project

--- CallbackSample/Language_C/CallbackSample/callbackSample/main.c
+++ CallbackSample/Language_C/CallbackSample/callbackSample/main.c
@@ -8,8 +8,40 @@
 
 #include <stdio.h>
 
+int mTemperatures []  = { 30, 20, 22, 18, 18, 15, 10, -5, 5, -8, 0, -1 };
+
+int mIsHeaterTurnedOn = 0;
+int handlerNum = 0;
+
+typedef void (*callback_temperature)(int temperature);
+callback_temperature callArr[1];
+
+
+void detectCurrentTemperature(int temperature) {
+    if(temperature < 0) {
+        printf("Now, the temperature is under 0 celsius. So I will turn on the heater. \n");
+        mIsHeaterTurnedOn = 1;
+    } else {
+        printf("The current temperature is greater than 0 celcius. I will turn the heater off. \n");
+        mIsHeaterTurnedOn = 0;
+    }
+};
+
+void registerCallback(callback_temperature handler) {
+    callArr[handlerNum] = handler;
+    handlerNum++;
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    registerCallback(detectCurrentTemperature);
+    
+    for (int i = 0; i< (sizeof(mTemperatures) / sizeof(int)) ; i++) {
+        printf("The current temperature :  %d \n " , mTemperatures[i]);
+        callArr[0](mTemperatures[i]);
+    }
+    
     return 0;
 }
+
+
+
